078ee766fbe6a6094f8fd7936bd0e52f8f24ddc6
yujiah-github <cil05265@naver.com>
2022-06-21 10:56:37 +0900

    Design: Add Pagination, HorizontalScrollIndicator, IndicatorStyle

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions, pagingEnabled } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width; // = cconst {width: SCREEN_WIDTH } = Dimensions.get('window');와 같다
 
 export default function App() {
   return (
@@ -7,17 +9,31 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Tokyo</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView
+        pagingEnabled //불필요한 스크롤 넘김을 방지하는 페이지 네이션 사용
+        horizontal
+        showsHorizontalScrollIndicator={true} //스크롤 인디케이터 표시 여부
+        indicatorStyle={'white'} //ios 에서만 동작
+        contentContainerStyle={styles.weather}>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
       <StatusBar></StatusBar>
     </View>
   );
 }
 
+//웹에서는 스크롤이 가능, 앱은 아님
 const styles = StyleSheet.create({
   //전체 컨테이너 CSS
   container: {
@@ -38,11 +54,12 @@ const styles = StyleSheet.create({
 
   //날씨 컨테이너 CSS
   weather:{
-    flex: 3,
+    
   },
 
+  //설정할 때 숫자 지양
   day:{
-    flex: 1,
+    width: windowWidth,
     alignItems:"center",
   },
 
@@ -56,3 +73,7 @@ const styles = StyleSheet.create({
   },
 });
 
+//웹에서는 스크롤이 가능, 앱은 아님
+//그래서 ScrollView 컴포넌트를 사용해야하고 수평으로 스크롤 하고 싶으면 Horizontal을 써준다
+// 그리고 스크롤뷰의 스타일은 contentContainerStyle을 사용해야 한다.
+// 스크롤 뷰에서 FLEX는 사용 안함
